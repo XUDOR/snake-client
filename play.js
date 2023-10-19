e92d9f95a5878574bfbeed85df0c62a4399093ab
Roderick Shoolbraid <rdxenv@gmail.com>
2023-10-18 22:00:21 -0700

    modular es6 shorthand

--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Successfully connected to the server!");
-  });
-
-  conn.on("data", (data) => {
-    console.log("Received from server:", data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
